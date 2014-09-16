299bda09613fa9ad3f8b5eb0c5323d980553fe71
Oscar Friberg <friberg.oscar@hotmail.com>
2014-09-16 10:29:43 +0200

    massa grejer
    en del ska bröt

---
+++ AGI/Assets/Scripts/IKOscar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IKOscar : MonoBehaviour {
+	private Animator animator;
+
+	public Transform objToPickUp;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+//	void OnAnimatorIK(int ){
+//				float reach = animator.GetFloat ("RightHandReach");
+//				animator.SetIKPositionWeight (AvatarIKGoal.RightHand, reach);
+//				animator.SetIKPosition (AvatarIKGoal.RightHand, objToPickUp.position);
+		}
+
+//}
