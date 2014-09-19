c733f18c1ee75b0ffe6fb68c0cdf61d62b4b66ff
Oscar Friberg <friberg.oscar@hotmail.com>
2014-09-19 15:47:19 +0200

    Jord, particles system

---
+++ AGI/Assets/Scripts/EarthRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthRotation : MonoBehaviour {
+	public GameObject Earth;
+	public float RotHastighet;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Earth.transform.Rotate (0, RotHastighet*Time.deltaTime/10, 0);
+	}
+}
---
+++ AGI/Assets/Scripts/PlanetCameraRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetCameraRotate : MonoBehaviour {
+	public Transform PlayerCamera;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.rotation = PlayerCamera.transform.rotation;
+	}
+}
