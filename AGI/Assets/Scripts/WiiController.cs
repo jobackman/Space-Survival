6c11d1458a5579f2c564b375d2bc7d84d892c81f
Stefan Etoh <stefan.etoh@gmail.com>
2014-09-20 17:01:33 +0200

    Supah movements

--- AGI/Assets/Scripts/WiiController.cs
+++ AGI/Assets/Scripts/WiiController.cs
@@ -4,66 +4,130 @@ using System.Collections;
 using System.Runtime.InteropServices;
 
 public class WiiController : MonoBehaviour {
-
-	[DllImport ("UniWii")]
-	private static extern int wiimote_count();
 	
-	[DllImport ("UniWii")]
-	private static extern float wiimote_getRoll(int which);
-	
-	[DllImport ("UniWii")]
-	private static extern float wiimote_getPitch(int which);
-	
-	[DllImport ("UniWii")]
-	private static extern float wiimote_getYaw(int which);
-
 	[DllImport("UniWii")]
 	private static extern Boolean wiimote_getButtonA(int which);
 	
 	[DllImport("UniWii")]
 	private static extern Boolean wiimote_getButtonB(int which);
 	
-	private GameObject player;
-
+	[DllImport ("UniWii")]
+	private static extern float wiimote_getRoll(int which);
+	[DllImport ("UniWii")]
+	private static extern float wiimote_getPitch(int which);
+	[DllImport ("UniWii")]
+	private static extern float wiimote_getYaw(int which);
+	
+	[DllImport ("UniWii")]
+	private static extern int wiimote_count();
+	
+	public GameObject player;
+	public GameObject backLeftThruster;
+	public GameObject backRightThruster;
+	public GameObject centerLeftThruster;
+	public GameObject centerRightThruster;
+	public GameObject sideLeftThruster;
+	public GameObject sideRightThruster; 
+	
 	private float yaw;
 	private float roll;
 	private float pitch;
-
+	private float fuel = 10000;
+	private bool ARight;
+	private bool BRight;
+	private bool ALeft;
+	private bool BLeft;
+	private float force = 10;
+	
 	// Use this for initialization
 	void Start () {
-		player = GameObject.Find("/Player/");
+		//player = GameObject.Find("/PlayerCube/");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-		if(wiimote_count() != 0){
-
-			roll = wiimote_getRoll(0);
-			yaw = wiimote_getYaw (0);
-			pitch = wiimote_getPitch(0);
-
-			if(wiimote_getButtonA(0) && roll <= -60)
+	void FixedUpdate () {
+		//when wiimotes fuck up
+		//BRight = Input.GetKey(KeyCode.RightArrow);
+		//BLeft = Input.GetKey(KeyCode.LeftArrow);
+		
+		int count = wiimote_count ();
+		if(count > 0){
+			
+			roll = GetComponent<UniWiiCheck>().roll;
+			//yaw = GetComponent<UniWiiCheck>().yaw;
+			pitch = GetComponent<UniWiiCheck>().pitch;
+			
+			ARight = wiimote_getButtonA(0);
+			BRight = wiimote_getButtonB(0);
+			BLeft = wiimote_getButtonB(1);
+			
+			//ROLL vänster
+			if(ARight && roll <= -60)
 			{
-				player.rigidbody.AddRelativeTorque(0f,0f,1f, ForceMode.Impulse);	
+				//player.rigidbody.AddRelativeTorque(0f,0f,0.1f, ForceMode.Impulse);
+				sideRightThruster.rigidbody.AddForceAtPosition(sideRightThruster.transform.up * force, sideRightThruster.transform.position);
+				sideLeftThruster.rigidbody.AddForceAtPosition(-sideLeftThruster.transform.up * force, sideLeftThruster.transform.position);
+				
+				fuel=fuel-1;
 			}
-			if(wiimote_getButtonA(0) && roll >= 30)
+			//ROLL höger
+			if(ARight && roll >= 30)
 			{
-				player.rigidbody.AddRelativeTorque(0f,0f,-1f, ForceMode.Impulse);	
+				//player.rigidbody.AddRelativeTorque(0f,0f,-0.1f, ForceMode.Impulse);
+				sideLeftThruster.rigidbody.AddForceAtPosition(sideLeftThruster.transform.up * force, sideLeftThruster.transform.position);
+				sideRightThruster.rigidbody.AddForceAtPosition(-sideRightThruster.transform.up * force, sideRightThruster.transform.position);
+				
+				fuel=fuel-1;
 			}
-			if (wiimote_getButtonB(0)) 
+			
+			//SPINN -> B-button
+			if (BRight &! BLeft)
 			{
-				player.rigidbody.AddRelativeForce(0f,0f,1f, ForceMode.Impulse);
-				//player.rigidbody.AddForceAtPosition(player.transform.forward * 10f, player.transform.position);
+				//player.rigidbody.AddRelativeForce(0f,0f,1f, ForceMode.Impulse);
+				
+				
+				centerLeftThruster.rigidbody.AddForceAtPosition(-centerLeftThruster.transform.up * force, centerLeftThruster.transform.position);
+				centerRightThruster.rigidbody.AddForceAtPosition(centerRightThruster.transform.up * force, centerRightThruster.transform.position);
+				fuel=fuel-1;
 			}
-			if(wiimote_getButtonA(0) && pitch >= 45)
+			if (BLeft &! BRight) 
 			{
-				player.rigidbody.AddRelativeTorque(1,0f,0f, ForceMode.Impulse);	
+				//player.rigidbody.AddRelativeForce(0f,0f,1f, ForceMode.Impulse);
+				
+				centerLeftThruster.rigidbody.AddForceAtPosition(centerLeftThruster.transform.up * force, centerLeftThruster.transform.position);
+				centerRightThruster.rigidbody.AddForceAtPosition(-centerRightThruster.transform.up * force, centerRightThruster.transform.position);
+				fuel=fuel-1;
 			}
-			if(wiimote_getButtonA(0) && pitch <= -45)
+			if(BRight && BLeft){
+				centerLeftThruster.rigidbody.AddForceAtPosition(centerLeftThruster.transform.up * force, centerLeftThruster.transform.position);
+				centerRightThruster.rigidbody.AddForceAtPosition(centerRightThruster.transform.up * force, centerRightThruster.transform.position);
+				fuel=fuel-1;
+			}
+			
+			
+			//PITCH framåt
+			if(ARight && pitch >= 45)
 			{
-				player.rigidbody.AddRelativeTorque(-1f,0f,0f, ForceMode.Impulse);	
+				//player.rigidbody.AddRelativeTorque(0.1f,0f,0f, ForceMode.Impulse);	
+				backLeftThruster.rigidbody.AddForceAtPosition(backLeftThruster.transform.up * force, backLeftThruster.transform.position);
+				backRightThruster.rigidbody.AddForceAtPosition(backRightThruster.transform.up * force, backRightThruster.transform.position);
+				fuel=fuel-1;
+			}
+			
+			//PITCH bakåt
+			if(ARight && pitch <= -45)
+			{
+				//player.rigidbody.AddRelativeTorque(-0.1f,0f,0f, ForceMode.Impulse);	
+				
+				backLeftThruster.rigidbody.AddForceAtPosition(-backLeftThruster.transform.up * force, backLeftThruster.transform.position);
+				backRightThruster.rigidbody.AddForceAtPosition(-backRightThruster.transform.up * force, backRightThruster.transform.position);	
+				fuel=fuel-1;
 			}
 		}
+	}
+	
+	void OnGUI()
+	{
+		GUI.Label( new Rect(100,10, 500, 100), "Fuel left: " + fuel);
 	}
 }
