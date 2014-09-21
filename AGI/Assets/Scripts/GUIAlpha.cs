e62fda718fd06da2769e9a60d29e7a77cb618bf2
Oscar Friberg <friberg.oscar@hotmail.com>
2014-09-22 00:07:25 +0200

    Glas, imma och breath. Ligger i andra scenen

---
+++ AGI/Assets/Scripts/GUIAlpha.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIAlpha : MonoBehaviour {
+
+	public float Transparancy = 0.1f;
+	void OnGUI()
+	{
+		Color textureColor = guiTexture.color;
+		textureColor.a = Transparancy;
+		
+		guiTexture.color = textureColor;
+	}
+	// Update is called once per frame
+
+}
---
+++ AGI/Assets/Scripts/GlasImma.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlasImma : MonoBehaviour {
+	public float AndningsHastighet = 2.0f;
+	public AudioClip Breath;
+	float timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnGUI()
+	{
+		Color textureColor = guiTexture.color;
+		textureColor.a = Mathf.Sin(Time.time * AndningsHastighet);
+		guiTexture.color = textureColor;
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+		timer = Mathf.Sin(Time.time * AndningsHastighet);
+		timer = Mathf.Abs (timer);
+		Debug.Log (timer);
+		if (timer >= 0f && timer <= 0.02f) {
+			audio.PlayOneShot(Breath);
+		}
+	}
+}
+
