79f45d952f78e37ca2dfb6d77a7d7f1b701be82a
Oscar Friberg <friberg.oscar@hotmail.com>
2014-09-23 13:28:31 +0200

    Glas till kameran

---
+++ AGI/Assets/Scripts/HelmetGlassScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelmetGlassScreen : MonoBehaviour {
+	public Texture GuiTexture;
+	private GUITexture TurnON;
+	float X;
+	float W;
+	
+	float Y;
+	float H;
+
+	void Start(){
+		TurnON = gameObject.GetComponent<GUITexture>();
+		TurnON.enabled = true; 
+		}
+
+	void OnGUI() {
+
+		W = Screen.width;
+		H = Screen.height;
+
+		X = -W / 2;
+		Y = -H / 2;
+				guiTexture.pixelInset = new Rect (X, Y, W, H);
+		}
+
+
+
+}
