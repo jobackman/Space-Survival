f0c3fb18c4d6237a0f3e67b3182676e8832eb292
Oscar Friberg <friberg.oscar@hotmail.com>
2014-10-23 23:07:43 +0200

    Thrusterljud (i TestArea-Oscar-scenen)

---
+++ AGI/Assets/Scripts/ThrusterSound.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrusterSound : MonoBehaviour {
+
+	public AudioClip WN;
+	public AudioClip WNfadeOut;
+	public AudioSource WNAudioSource;
+
+	void Start () {
+			audio.clip = WN;
+			WNAudioSource.playOnAwake = true;
+			WNAudioSource.enabled = false;
+			WNAudioSource.loop = false;
+
+		}
+
+	IEnumerator turnOffWN(){
+			WNAudioSource.loop = false;
+			audio.PlayOneShot(WNfadeOut);
+			yield return new WaitForSeconds(WNfadeOut.length);
+			WNAudioSource.enabled = false;
+			}
+
+	void Update () {
+		 
+			if (Input.GetKey(KeyCode.E))
+				{
+					WNAudioSource.enabled = true;
+					WNAudioSource.loop = true;
+				}
+
+			
+			if (Input.GetKeyUp (KeyCode.E))
+				{
+					StartCoroutine(turnOffWN());
+				}
+
+		}
+}
